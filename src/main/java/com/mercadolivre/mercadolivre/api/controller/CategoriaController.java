348686b44506f70b87d37a2106b465ed23ae94f2
thaismathias <thais-mathias@hotmai.com>
2020-10-19 12:03:22 -0300

    Update CategoriaController.java
    -Excluindo import não usado em categoria controller

--- src/main/java/com/mercadolivre/mercadolivre/api/controller/CategoriaController.java
+++ src/main/java/com/mercadolivre/mercadolivre/api/controller/CategoriaController.java
@@ -2,7 +2,6 @@ package com.mercadolivre.mercadolivre.api.controller;
 
 import com.mercadolivre.mercadolivre.api.model.CategoriaRequest;
 import com.mercadolivre.mercadolivre.model.Categoria;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,12 +23,9 @@ public class CategoriaController {
     @PostMapping
     @Transactional
     public ResponseEntity<Void> ciarCategoria(@RequestBody @Valid CategoriaRequest request) {
-
         //+1 CDD Categoria
         Categoria categoria = request.toModel(manager);
         manager.persist(categoria);
-
-
         return ResponseEntity.ok().build();
     }
 }
